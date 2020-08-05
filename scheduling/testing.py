86e49671759ec9c686fffe7e00614bcd75be3b13
mathewsmonoo <m.monoo@aluno.ifsp.edu.br>
2020-08-05 18:28:45 -0300

    some progress in class allocation

--- scheduling/testing.py
+++ scheduling/testing.py
@@ -32,9 +32,9 @@ class Time:
     #The following method is just for printing, not returning useful data
     def get_isOccupiedStr(self):
         if self._isOccupied:
-            return f"Available/Empty"
+            return f"Occupied"
         else:
-            return f"Occupied/Full"
+            return f"Available"
     
     def change_isOccupied(self):  self._isOccupied = not(self._isOccupied)
     def __str__(self):   return f'{self.get_time()} - {self.get_isOccupiedStr()}'
@@ -64,7 +64,7 @@ class Day: # Day Object
     def get_day(self):
         holder = ''
         for time in self._times:
-            holder += str(time) + "\n"
+            holder += str(time.get_time()) + str(time.) 
         return holder
     
     def get_times(self):
@@ -100,73 +100,48 @@ def create_week(): #Creates Week
         myWeek.append(d)
     return myWeek
 
-def doit(): # Runner
-#    week    = create_week()
-#    myWeek  = allocate_rooms(week)
-#    for day in week:    print(day)
-#    print('-'*50,)
-    a,b = test()
-    for each in a:
-        print(each.get_time(),' ',each.get_room())
-    print(b)
 
-def doita(): # Runner
-    a,b = testa()
-    for each in a:
-        print(each.get_time(),' ',each.get_room())
-    print(b)
-
-def testa():
-    counter = 0 
+def generateClasses():
+    counter = 0               # counter for generating Class Id 
     classList = []
-    rejectedCounter = 0
-    myDay = Day(1,'MON')
-    times = myDay.get_times()
+    rejectedCounter = 0       # counter of rejected iterations
+    myDay = Day(1,'MON')      # "MON" is just a holder
+    times = myDay.get_times()  # list with all times in that day
 
     # Here we sort ROOMS and COURSES by most capacity and slots, respectively.
-    sorted_courses  = sorted(COURSES, key = lambda x: x._slots, reverse = True) 
     sorted_rooms    = sorted(ROOMS,   key = lambda y: y[1],     reverse = True)
-
-    for time in times:
-        if (time.get_isOccupied() == False):       # If time is not occupied
-            for room in sorted_rooms:
-                for course in sorted_courses:
-                    course_slots  = course.get_slots()      #
-                    room_capacity = room[1]        # room is a list and [1] is the capacity
-                    if (room_capacity >= course_slots):
-                        time.change_isOccupied()
-                        counter += 1 
-                        newClass = Class(counter, course, time, room)
-                        classList.append(newClass)
-                        break
-                    else:
-                        rejectedCounter += 1
-        else:                                       # If time is occupied
-            pass
-    return classList,rejectedCounter
-
-def test():
-    counter = 0 
-    classList = []
-    rejectedCounter = 0
-    myDay = Day(1,'MON')
-    times = myDay.get_times()
-
     sorted_courses  = sorted(COURSES, key = lambda x: x._slots, reverse = True) 
-    sorted_rooms    = sorted(ROOMS,   key = lambda y: y[1],     reverse = True)
-    
+
+    occupied_rooms = {}       # Dictonary created to control which rooms are occupied
     for time in times:
         for course in sorted_courses:
-            for room in sorted_rooms:
-                if (sorted_rooms[0][1] >= course.get_slots()):
-                    if (time.get_isOccupied() == False):
+            if (time.get_isOccupied() == False):       # If time is not occupied
+                for room in sorted_rooms:
+                  if room[0] not in occupied_rooms:    # If the room is not occupied 
+                    course_slots  = course.get_slots() 
+                    room_capacity = room[1]        # room is a list and [1] is the capacity
+                    if (room_capacity >= course_slots):
                         counter += 1 
-                        time.change_isOccupied()
                         newClass = Class(counter, course, time, room)
                         classList.append(newClass)
-                else:
-                    rejectedCounter += 1
+                        occupied_rooms[room[0]] = room[1]
+                        time.change_isOccupied()
+                        break
+            else:                                       # If time is occupied
+                rejectedCounter += 1
     return classList,rejectedCounter
+
+def doit(): # Runner
+    a,b = generateClasses()
+    print(f'{len(a)} Time Slots per day')
+    print(f'\t\tClasses:')
+    for each in a:
+        print(each.get_time(),' ',each.get_room())
+    print(f'Rejected iterations: {b}')
+
+    newDay = Day(1,'MON',a)
+    print(newDay.get_day())
+
 
 def allocate_rooms(myWeek):
     holderWeek = []
@@ -223,3 +198,5 @@ WEEK = {
     'FRI':None,
 }
 
+if __name__=="__main__":
+  doit()
