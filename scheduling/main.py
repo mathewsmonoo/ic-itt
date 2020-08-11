41adf1ed412b47baaee5938556e8410d7649acb4
mathewsmonoo <m.monoo@aluno.ifsp.edu.br>
2020-08-11 04:10:17 -0300

    Adding basic code for schedule fitness

--- scheduling/main.py
+++ scheduling/main.py
@@ -5,9 +5,11 @@ from random import randint
 from random import sample
 from random import shuffle
 from datetime import datetime
-#--------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
 # Classes
-#--------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
 class Room:
     def __init__(self, id, capacity):
         self._id        = id
@@ -16,7 +18,7 @@ class Room:
     def get_id(self):           return self._id
     def get_capacity(self):     return self._capacity
 
-    def __str__(self): 
+    def __str__(self):
         return  f'ROOM: {self.get_id()}\t' \
                 f'Capacity:{self.get_capacity()}\n\n'
 
@@ -25,12 +27,11 @@ class Course:
         self._id                = id
         self._students          = students
         self._timesPerWeek      = timesPerWeek
-
     def get_id(self):               return self._id
     def get_students(self):         return self._students
     def get_timesPerWeek(self):     return self._timesPerWeek
 
-    def __str__(self): 
+    def __str__(self):
         return  f'\nCourseID:{self.get_id()}\t' \
                 f'Students:{self.get_students()}\t' \
                 f'Times Per Week:{self.get_timesPerWeek()}\n'
@@ -46,13 +47,10 @@ class Time:
     def get_time(self):             return self._time
     def get_day(self):              return self._day
     def get_isAvailable(self):      return self._isAvailable
-    
     def set_Available(self):         self._isAvailable = True
     def set_Occupied(self):          self._isAvailable = False
-    
 
-
-    def __str__(self): 
+    def __str__(self):
         return  f'TimeID:{self.get_id()}\n' \
                 f'{self.get_time()}\t' \
                 f'Occupied:{self.get_isAvailable()}'
@@ -66,8 +64,8 @@ class Class:
 
     def get_id(self):         return self._id
     def get_course(self):     return self._course
-    def get_time(self):       return self._time  
-    def get_room(self):       return self._room  
+    def get_time(self):       return self._time
+    def get_room(self):       return self._room
 
     def set_course(self, course):   self._course  = course
     def set_time(self, time):       self._time    = time
@@ -81,56 +79,69 @@ class Class:
 
 class Schedule:
     def __init__(self, id):
-        self._id                 = id
-        self._times              = []
-        self._listOfClasses      = []
-        self._conflicts          = 0
-        self._classCounter      =  0
-
+        self._id                = id
+        self._times             = []
+        self._listOfClasses     = []
+        self._conflicts         = 0
+        self._classCounter      = 0
+        self._fitness           = -1
         for i in (TIMES):
             self._times.append(i)
 
-    def get_id(self):       return self._id    
-    def get_times(self):    return self._times 
+    def get_id(self):           return self._id
+    def get_times(self):        return self._times
+    def get_conflicts(self):    return self._conflicts
+    def get_fitness(self):      return self._fitness
     
+    def calculate_fitness(self):
+        self._fitness = (1 / ((1.0 * self.get_conflicts()) + 1))
+
     def get_listOfClasses(self):
         return self._listOfClasses
-    
+
     def get_listOfClassesStr(self):
         holder = ""
         for each in self._listOfClasses:
             holder += str(each.get_time()) + str(each.get_course()) + str(each.get_room())
         return holder
-    
+
     def add_class(self, myClass):
         self._listOfClasses.append(myClass)
         self._classCounter += 1
-       
-    # This method perform checks looping over Courses(times per week), Times (availability) and Rooms (capacity) 
+
+    def add_conflict(self):     self._conflicts += 1
+    
+    # This method perform checks looping over Courses(times per week), Times (availability) and Rooms (capacity)
     # and appends a new class to the Schedule if everything is ok.
     def initialize(self):
         counter         = 0
         roomList        = sorted(ROOMS,   key = lambda y: y._capacity,  reverse = True)
         coursesList     = sorted(COURSES, key = lambda x: x._students,  reverse = True)
-        for course in coursesList:                                  # loop over courses
-            for i in range(course.get_timesPerWeek()):              # how many classes per week
-                for time in self._times:                            # loop and verify each time
-                    if (time.get_isAvailable()):                     # for availability
-                        for room in roomList:                       
-                            if (course.get_students() <= room.get_capacity()): # check capacity for each room
+        for course in coursesList:
+            # loop over courses
+            for i in range(course.get_timesPerWeek()):
+                # how many classes per week
+                for time in self._times:
+                    # loop and verify each time for availability
+                    if (time.get_isAvailable()):
+                        for room in roomList:
+                            if (course.get_students() <= room.get_capacity()):
+                                # check capacity for each room
                                 time.set_Occupied()
                                 myClass = Class(counter,course,time,room)
-                                counter += 1                        #id counter
+                                counter += 1
+                                #id counter
                                 self.add_class(myClass)
                             else:
-                                self._conflicts += 1
+                                self.add_conflict()
                             break
                         break
                     else:
-                        self._conflicts += 1
+                        self.add_conflict()
                     time.set_Available()
+        
         return self
-    
+
     def sort_classes(self):
         self._listOfClasses.sort(key = lambda x : x._time.get_id())
 
@@ -148,27 +159,12 @@ class Schedule:
                                   f'{eachClass._room.get_id()})',
                                   end = "\t\t ")
             print('\n','-'*130)
+            
+        print(f'Conflicts: {self.get_conflicts()}')
 
-    def print_grid_with_allocation(self):
-        for day in DAYS:
-            print("\t\t  ",day,end = "")
-        print("\n")
-        for time in TIMESSTR:
-            print(time,end = "\t")
-            for day in DAYS:
-                for eachClass in self._listOfClasses:
-                    if eachClass._time.get_time() == time:
-                        if eachClass._time.get_day() == day:
-                            print(f'{eachClass._course.get_id()}:'\
-                                  f'{eachClass._course.get_students()} '\
-                                  f'{eachClass._room.get_id()}:'\
-                                  f'{eachClass._room.get_capacity()}',
-                                  end = "  \t")
-            print('\n','-'*150)
-
-    def __str__(self): 
+    def __str__(self):
         return str(self.print_grid())
-                
+
                 
 class AllRndSchedule(Schedule):
     def initialize(self):
@@ -181,31 +177,58 @@ class AllRndSchedule(Schedule):
         self._times     = create_times(DAYS,TIMESSTR)
         shuffle(self._times)
 
+        occupiedTimes = {}
         for course in rndCoursesList:
-            for i in range(course.get_timesPerWeek()):#4x
-                for time in self._times:              #20x       # loop and verify each time
+            for i in range(course.get_timesPerWeek()):
+                for time in self._times:
+                                                        # loop and verify each time
                     t = time.get_id()
-                    if (time.get_isAvailable()):                        # for availability
-                        for room in rndRoomList:      #4x
-                            if (course.get_students() <= room.get_capacity()): # check capacity for each room
+                    if (t not in occupiedTimes):
+                        for room in rndRoomList: 
+                            if (course.get_students() <= room.get_capacity()):
+                                                        # check capacity for each room
                                 time.set_Occupied()
-                                myClass = Class(counter,course,time,room)
-                                counter += 1                        #id counter
+                                occupiedTimes[t] = t
+                                myClass = Class(counter, course, time, room)
                                 self.add_class(myClass)
+                                counter += 1            # id counter
                                 try:
                                     nextTime = self._times[t+1]
-                                    if(nextTime.get_isAvailable()):
-                                        print("TestOK")
-                                        myDoubleClass = Class(counter,course,nextTime._time.get_id(),room)
+                                    if(nextTime._time.get_id() not in occupiedTimes):
+                                        myDoubleClass = Class(counter, course, nextTime._time.get_id(), room)
                                         nextTime.set_Occupied()
-                                        counter+=1
+                                        occupiedTimes[nextTime._time.get_id()] = nextTime._time.get_id()
                                         self.add_class(myDoubleClass)
+                                        counter+=1
+                                    else:
+                                        self.add_conflict()
+                                        break
                                     break
                                 except:
                                     pass
-                                break   # breaks room loop by the next time availability check
+                                break
+                                # breaks room loop by the next time availability check
+                            else:
+                                self.add_conflict()
                         break           # breaks range(loop)
+                    else:
+                        self.add_conflict()
+        self.calculate_fitness()
         return self
+
+class Population:
+    def __init__(self, id, schedulesList = []):
+        self._id        = id
+        self._schedules = schedulesList
+    
+    def get_id(self):           return self._id
+    def get_schedules(self):    return self._schedules
+    
+    def print_schedules(self):
+        for each in self._schedules:
+            each.print_grid()
+            print(each.get_fitness())
+
 
 #--------------------------------------------------------------------------------
 # Functions
@@ -214,8 +237,8 @@ def create_times(DAYS,TIMESSTR):
     # generates time list from TIMESSTR
     times   = []
     counter = 0
-    for i in range(len(DAYS)):     # 5 days
-        for j in range(len(TIMESSTR)): # 6 times per day
+    for i in range(len(DAYS)):          # 5 days
+        for j in range(len(TIMESSTR)):  # 6 times per day
             counter += 1
             times.append(Time(counter,TIMESSTR[j], DAYS[i]))
     return times
@@ -227,12 +250,11 @@ def create_rand_schedule(id):
     return allRndSchedule
 
 def create_population(size):
-    myPopulation = []
+    schedulesList = []
     for i in range(size):
         myIndividual = create_rand_schedule(i)
-        myPopulation.append(myIndividual)
-    return myPopulation
-        
+        schedulesList.append(myIndividual)
+    return schedulesList
 
 #--------------------------------------------------------------------------------
 # Sample Data
@@ -275,17 +297,14 @@ ROOMS = [
 #   Runner Code
 #--------------------------------------------------------------------------------
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Single Schedule Creation
     allRndSchedule = create_rand_schedule(1)
     print("*"*150, "\n\tSchedule\n","*"*150)
     allRndSchedule.print_grid()
-    print("*"*150, "\n\tSchedule with Allocation Info\n","*"*150)
-    allRndSchedule.print_grid_with_allocation()
 
     # Population of Schedules Creation
-    myPop = create_population(100)
-    for each in myPop:
-        print('\n',('*'*130))
-        print(f'Specimen #{each.get_id()}')
-        each.print_grid()
+    myPop = Population(1,create_population(100))
+    for each in myPop.get_schedules():
+        print(each.get_fitness())
+   
